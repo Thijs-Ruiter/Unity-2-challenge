049592ab6ed197cc50acd43e212d6861228560a3
Thijs <71495073+Thijs-Ruiter@users.noreply.github.com>
2022-11-28 15:30:05 +0100

    :)

---
+++ PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float cooldown = 2;
+    private float timeStamp = 0;
+    private float currentCooldown;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timeStamp <= Time.time){
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeStamp = Time.time + cooldown;
+        }
+    }
+}
